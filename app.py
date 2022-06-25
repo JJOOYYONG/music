c67eaa93c9242794510f0f82e0a9bca6f7145e28
JJOOYYONG <54390853+JJOOYYONG@users.noreply.github.com>
2022-06-26 02:20:23 +0900

    2022.06.26 1.예저 css/js 붙여넣기 2.웹크롤링 url input 박스 생성 3.웹크롤링 db 저장 후 화면 노출 4.예제어서 화면 노출되는 css 변경

--- app.py
+++ app.py
@@ -5,9 +5,51 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:sparta@cluster0.vygyl.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+
+
 @app.route('/')
 def home():
    return render_template('index.html')
 
+
+
+@app.route('/save_url', methods=['POST'])
+def save_post():
+   url_receive = request.form['url_give']
+
+   data = requests.get(url_receive, headers=headers)
+   soup = BeautifulSoup(data.text, 'html.parser')
+
+
+   og_img = soup.select_one('meta[property="og:image:secure_url"]')
+
+   og_info = soup.select_one('meta[property="og:title"]')
+   og_title = og_info['content'].split('/')[0]
+   og_artist = og_info['content'].split('/')[1].split('- genie')[0]
+   print(og_info,og_title,og_artist)
+   image = og_img['content']
+   title = og_title
+   artist = og_artist
+
+   doc = {
+      'img':image,
+      'title':title,
+      'artist':artist
+   }
+   db.music.insert_one(doc)
+   return jsonify({'result':'success', 'msg': '등록완료'})
+
+@app.route('/get_list', methods=['GET'])
+def get_list():
+   all_music = list(db.music.find({}, {'_id': False}))
+   return jsonify({'result': 'success', 'list': all_music})
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
+
+
--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,141 @@
 const frame =document.querySelector("section");
-console.log(frame);
 const lists = frame.querySelectorAll("article");
 const deg = 45;
 const len =lists.length-1;
 let i = 0;
 
-for(let el of lists){
-    let pic = el.querySelector(".pic");
-    el.style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
-    pic.style.backgroundImage=`url(../static/img/member${i+1}.jpg)`;
-    i++
+
+$(document).ready(function (){
+   getlist();
+});
+
+// for (let el of lists) {
+//     let pic = el.querySelector(".pic");
+//     el.style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
+//     // pic.style.backgroundImage=`url(../static/img/member${i+1}.jpg)`;
+//     i++
+// }
+function play(e){
+    let isActive = $(e).currentTarget.closest("article").querySelector(".pic").classList.add("on");
+    if(isActive){
+        $(e).currentTarget.closest("article").querySelector("audio").play();
+    }
+
+}
+function pause(e){
+    let isActive = $(e).currentTarget.closest("article").querySelector(".pic").classList.remove("on");
+    if(isActive) {
+        $(e).currentTarget.closest("article").querySelector("audio").pause();
+    }
 }
 
-function play(e){
-    e.parnet
+function load(e){
+     let isActive = $(e).currentTarget.closest("article").querySelector(".pic").classList.add("on");
+     if(isActive){
+         $(e).currentTarget.closest("article").querySelector("audio").load();
+         $(e).currentTarget.closest("article").querySelector("audio").play();
+     }
+}
+
+
+let num = 0 ;
+let active = 0;
+
+function activation(index,lists){
+    for (let el of lists){
+        el.classList.remove("on");
+    }
+    lists[index].classList.add("on");
+}
+
+const audio = frame.querySelectorAll("audio");
+
+function initMusic(){
+    for(let el of audio){
+        el.pause();
+        el.load();
+        el.parentElement.previousElementSibling.classList.remove("on");
+    }
+}
+
+
+function prev(){
+    initMusic();
+    num++;
+    frame.style.transform=`rotate(${deg*num}deg)`;
+    (active == 0)?active = len : active--;
+    activation(active,lists);
+}
+function next(){
+    initMusic();
+    num--;
+    frame.style.transform=`rotate(${deg*num}deg)`;
+    (active == 0)?active = len : active++;
+    activation(active,lists);
+}
+
+function save_url(){
+    let url = $("#music_url").val();
+    console.log(url)
+    $.ajax({
+       url:"/save_url",
+       data:{'url_give':url},
+       type:"POST",
+       success:function (result){
+            alert(result['msg']);
+            window.location.reload()
+        }
+    });
+}
+
+function getlist(){
+    $.ajax({
+       url:'/get_list',
+       data:{},
+       type:'GET',
+       success:function (result){
+           let data = result['list'];
+           let temp='';
+           if(data.length ==0){
+                temp =`<article>
+                          <span>등록된 곡이 없습니다 X-X</span>
+                       </article>`
+               $("#wrap").append(temp);
+           }else{
+
+
+                for(let i=0;i<data.length;i++){
+                    let img =data[i]["img"];
+                    let title = data[i]["title"];
+                    let artist = data[i]["artist"];
+
+                    temp=`<article style="transform:rotate(${deg * i}deg) translateY(-100vh)">
+                            <div class="inner">
+                                <div class="pic" style="background-image: url('${img}')">
+                                    <div class="dot"></div>
+                                </div>
+                                <div class="txt">
+                                    <h2>${title}</h2>
+                                    <p>${artist}</p>
+                                    <ul>
+                                        <li class="pause">
+                                            <i class="fas fa-pause" onclick="pause(this);"></i>
+                                        </li>
+                                        <li class="play">
+                                            <i class="fas fa-play" onclick="play(this);"></i>
+                                        </li>
+                                        <li class="load">
+                                             <i class="fas fa-redo-alt" onclick="load(this)"></i>
+                                        </li>
+                                    </ul>
+                                    <audio src ="../static/music/Blizzards.mp3"></audio>
+                                </div>
+                            </div>
+                        </article>`
+                    $("#wrap").append(temp);
+                }
+           }
+
+        }
+    });
 }
